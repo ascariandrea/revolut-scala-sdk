4f39fe6b211713d073a769f2c471181893bece4d
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-03-22 11:34:36 +0100

    Fixed test accordingly to new client implementation using futures

--- src/test/scala/com/ascariandrea/revolut/sdk/AccountSpec.scala
+++ src/test/scala/com/ascariandrea/revolut/sdk/AccountSpec.scala
@@ -1,19 +1,26 @@
 package com.ascariandrea.revolut.sdk
 
+import akka.http.scaladsl.model.Uri
 import com.ascariandrea.revolut.sdk.client.Client
 import com.ascariandrea.revolut.sdk.models.Account
 import com.ascariandrea.revolut.sdk.test.MockRevolutServer
 import okhttp3.mockwebserver.MockWebServer
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class AccountSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+class AccountSpec
+    extends FlatSpec
+    with Matchers
+    with BeforeAndAfterAll
+    with ScalaFutures {
 
   val server: MockWebServer = MockRevolutServer()
   var accountClient: Accounts = null
 
   override protected def beforeAll(): Unit = {
     server.start()
-    accountClient = new Accounts(new Client(server.url("/api")))
+    accountClient = new Accounts(new Client(Uri(server.url("/api").toString)))
   }
 
   override protected def afterAll(): Unit = {
@@ -21,26 +28,33 @@ class AccountSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   it should "get all account" in {
-    val accountEither = accountClient.getAll().get()
-    accountEither should be('right)
-    accountEither.right.get.get should have length 3
+    accountClient.getAll().map { result =>
+      {
+        result should be('right)
+        result shouldBe List
+      }
+    }
+
   }
 
   it should "get an account" in {
-    val accountEither = accountClient.get("42").get()
-    accountEither should be('right)
-    accountEither.right.get should be(
-      Some(
-        Account(
-          "2a0d4d03-e26c-4159-9de1-c6bf3adfd8a1",
-          "Current GBP account",
-          100.0,
-          "GBP",
-          "active",
-          false,
-          "2017-06-01T11:11:11.0Z",
-          "2017-06-01T11:11:11.0Z",
-          "pocket"
-        )))
+    accountClient.get("42").map { result =>
+      {
+        result should be('right)
+        result shouldBe Some(
+          Account(
+            "2a0d4d03-e26c-4159-9de1-c6bf3adfd8a1",
+            "Current GBP account",
+            100.0,
+            "GBP",
+            "active",
+            false,
+            "2017-06-01T11:11:11.0Z",
+            "2017-06-01T11:11:11.0Z",
+            "pocket"
+          ))
+      }
+    }
+
   }
 }
