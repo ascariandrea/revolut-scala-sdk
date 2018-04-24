c258c46c19a8757c94203f39786864e7a39444d6
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-04-24 14:16:40 +0200

    Created Counterparty model and api file

---
+++ src/main/scala/com/ascariandrea/revolut/sdk/models/Counterparty.scala
@@ -0,0 +1,68 @@
+package com.ascariandrea.revolut.sdk
+package models
+
+import io.buildo.enumero.annotations.enum
+import io.buildo.enumero.{CaseEnum, CaseEnumSerialization}
+
+@enum trait ProfileType {
+  business
+  personal
+}
+
+@enum trait State {
+  created
+  deleted
+}
+
+@enum trait Type {
+  revolut
+  external
+}
+
+case class Counterparty(
+    id: String,
+    name: String,
+    phone: String,
+    profile_type: String,
+    country: String,
+    state: String,
+    `type`: Type,
+    created_at: String,
+    updated_at: String,
+    accounts: List[Account]
+)
+
+import io.circe._
+import io.circe.generic.semiauto._
+
+object Counterparty {
+  // // also works inlined
+  implicit val counterpartyDecoder: Decoder[Counterparty] =
+    deriveDecoder[Counterparty]
+  implicit val counterpartyEncoder: Encoder[Counterparty] =
+    deriveEncoder[Counterparty]
+
+  implicit final def encodeCaseEnum[T <: CaseEnum: CaseEnumSerialization]
+    : Encoder[T] =
+    Encoder.instance(
+      caseEnum =>
+        Json.fromString(
+          implicitly[CaseEnumSerialization[T]].caseToString(caseEnum)))
+
+  implicit final def decodeCaseEnum[T <: CaseEnum: CaseEnumSerialization]
+    : Decoder[T] =
+    Decoder.instance { c =>
+      {
+        println(s"Cursor: ${c.as[String]}")
+        c.as[String] match {
+          case Right(s) =>
+            implicitly[CaseEnumSerialization[T]].caseFromString(s) match {
+              case Some(caseEnum) => Right(caseEnum)
+              case None           => Left(DecodingFailure("CaseEnum", c.history))
+            }
+          case l @ Left(_) => l.asInstanceOf[Decoder.Result[T]]
+        }
+      }
+
+    }
+}
