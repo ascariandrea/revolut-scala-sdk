03202817f49322d01787e8a17d059cccdabea0bc
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-04-24 14:17:19 +0200

    Fixed previous tests, created new tests fro Counterparty entities

--- src/test/scala/com/ascariandrea/revolut/sdk/server/MockRevolutServer.scala
+++ src/test/scala/com/ascariandrea/revolut/sdk/server/MockRevolutServer.scala
@@ -1,6 +1,6 @@
 package com.ascariandrea.revolut.sdk.test
 
-import com.ascariandrea.revolut.sdk.models.Account
+import com.ascariandrea.revolut.sdk.models.{Account, Counterparty}
 import okhttp3.mockwebserver.{
   Dispatcher,
   MockResponse,
@@ -13,24 +13,34 @@ import io.circe.syntax._
 
 object MockRevolutServer {
 
-  val account = random[Account].asJson.toString()
-  val accounts = random[Account](10).toList.asJson.toString()
+  private val account = random[Account].asJson.toString()
+  private val accounts = random[Account](10).toList.asJson.toString()
 
-  val server: MockWebServer = new MockWebServer()
+  private val counterparty =
+    random[Counterparty].asJson(Counterparty.counterpartyEncoder).toString()
 
-  def apply(): MockWebServer = {
+  private val counterparties = random[Counterparty](10)
+    .map(c => c.asJson(Counterparty.counterpartyEncoder))
+    .toList
+    .toString()
+
+  def create(): MockWebServer = {
+    val server: MockWebServer = new MockWebServer()
     server.setDispatcher(new Dispatcher {
       override def dispatch(request: RecordedRequest): MockResponse = {
-        request.getPath() match {
+        request.getPath match {
           case "/api/accounts" =>
             new MockResponse().setResponseCode(200).setBody(accounts)
           case "/api/accounts/42" =>
             new MockResponse().setResponseCode(200).setBody(account)
+          case "/api/counterparties" =>
+            new MockResponse().setResponseCode(200).setBody(counterparties)
+          case "/api/counterparty/42" =>
+            new MockResponse().setResponseCode(200).setBody(counterparty)
           case _ => new MockResponse().setResponseCode(404).setBody("not found")
         }
       }
     })
     server
   }
-
 }
