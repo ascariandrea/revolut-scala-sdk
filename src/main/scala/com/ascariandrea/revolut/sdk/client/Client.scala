68c7fbb733eaf34340c8593031f5e65093b4b4d0
Andrea Ascari <dev.ascariandrea@gmail.com>
2018-05-08 10:33:24 +0200

    Added post method to client

--- src/main/scala/com/ascariandrea/revolut/sdk/client/Client.scala
+++ src/main/scala/com/ascariandrea/revolut/sdk/client/Client.scala
@@ -3,11 +3,17 @@ package client
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.Uri.Path
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes, Uri}
+import akka.http.scaladsl.model.{
+  HttpResponse,
+  StatusCodes,
+  Uri,
+  HttpEntity,
+  ContentTypes
+}
 import akka.http.scaladsl.{Http, model}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
-import io.circe.Decoder
+import io.circe.{Decoder, Json}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import io.circe.parser._
@@ -21,37 +27,55 @@ class Client(val baseUrl: Uri) {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   def get[T: Decoder](path: Path): Future[Either[HttpResponse, Option[T]]] = {
-    makeRequest(path)
-      .map((either: Either[HttpResponse, Option[String]]) =>
-        either.right.map(opt => opt.map(decode[T]).flatMap(_.toOption)))
+    makeRequest(path, model.HttpMethods.GET, None)
+      .map(serialize[T])
   }
 
   def getMany[T: Decoder](
       path: Path): Future[Either[HttpResponse, Option[List[T]]]] = {
-    makeRequest(path)
-      .map(
-        (either: Either[HttpResponse, Option[String]]) =>
-          either.right
-            .map(json => json.map(decode[List[T]]).flatMap(_.toOption)))
+    makeRequest(path, model.HttpMethods.GET, None)
+      .map(serialize[List[T]])
+  }
+
+  def post[T: Decoder](
+      path: Path,
+      data: Option[Json]): Future[Either[HttpResponse, Option[T]]] = {
+    makeRequest(path, model.HttpMethods.POST, data)
+      .map(serialize[T])
   }
 
   private def makeRequest(
-      path: Path): Future[Either[HttpResponse, Option[String]]] = {
+      path: Path,
+      method: model.HttpMethod,
+      data: Option[Json]): Future[Either[HttpResponse, Option[String]]] = {
 
     val requestUri = Uri(
       baseUrl
         .withPath(baseUrl.path ++ path)
         .toString())
 
-    val responseFuture =
-      Http().singleRequest(model.HttpRequest(uri = requestUri))
-    responseFuture.flatMap {
-      case HttpResponse(StatusCodes.OK, _, entity, _) => {
-        Unmarshal(entity).to[String].map(s => Right(Some(s)))
-      }
-      case resp @ HttpResponse(_, _, _, _) => Future.apply(Left(resp))
-    }
+    Http()
+      .singleRequest(
+        model.HttpRequest(
+          uri = requestUri,
+          method = method,
+          entity = data
+            .map(d => {
+              HttpEntity(ContentTypes.`application/json`, d.toString())
+            })
+            .getOrElse(HttpEntity.empty(ContentTypes.`application/json`))
+        )
+      )
+      .flatMap {
+        case HttpResponse(StatusCodes.OK | StatusCodes.Created, _, entity, _) =>
+          Unmarshal(entity).to[String].map(s => Right(Some(s)))
 
+        case resp @ HttpResponse(_, _, _, _) => Future.apply(Left(resp))
+      }
   }
+
+  private def serialize[T: Decoder](
+      either: Either[HttpResponse, Option[String]]) =
+    either.right.map(json => json.map(decode[T]).flatMap(_.toOption))
 
 }
